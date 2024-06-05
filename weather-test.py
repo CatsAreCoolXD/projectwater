59f74887de9e216d5ad4b48cd064cddfc1a5b1fb
D K <dylan@dylankooistra.nl>
2024-06-05 15:33:21 +0200

    a

--- weather-test.py
+++ weather-test.py
@@ -1,37 +1,8 @@
-import python_weather
-import os
-import asyncio
-import overpy
-from geopy.geocoders import Nominatim
+import cgitb
+cgitb.enable()
 
-async def getweather():
-    
+def main():
+    return "test"
 
-    loc = Nominatim(user_agent="Geopy Library")
-
-    getLoc = loc.geocode("İzmir")
-
-    print(getLoc.address)
-
-    print("Latitude = ", getLoc.latitude, "\n")
-    print("Longitude = ", getLoc.longitude)
-
-    
-    osm = overpy.Overpass()
-    weather = await client.get(input("In welke stad woon je?/n"))
-    gebied = float(input("Hoeveel m2 is je dak?"))
-    stadGrootte = float(219.3*1000)
-
-    osm
-    
-    factor = gebied/stadGrootte
-
-    print(f"Het gaat {weather.precipitation*30} mm regenen vandaag.")
-    print(f"Jouw huis vangt {factor*weather.precipitation} mm water op")
-
-
-if __name__ == '__main__':
-  if os.name == 'nt':
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-  
-  asyncio.run(getweather())
+if __name__ =="__main__":
+    main()
