21b0bc36fbd3d967c6d2a6cbfc8e76ac7a4f5833
D K <dylan@dylankooistra.nl>
2024-06-05 15:47:40 +0200

    avayut

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function getData(){
-    
+    $.ajax({
+        url: "http://api.weatherapi.com/v1/current.json?key=be3b9fcb975540c2bb4132502240506&q=Amsterdam&aqi=no",
+        method: "GET"
+    }).done(function(data){
+        document.getElementById("output").innerHTML = data;
+    });
 }
 
 
