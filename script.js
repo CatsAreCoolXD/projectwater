a64760b26567d36bea4fc67246e4feaf199ec44d
LukeStraathof <131240985+LukeStraathof@users.noreply.github.com>
2024-06-18 13:40:34 +0200

    vvfsvsf

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ function getData(){
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-  }
+}
   
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
